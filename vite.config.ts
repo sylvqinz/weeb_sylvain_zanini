461e2c22b01b9ac082eb1b76c275710c4a7edea9
sylvqinz <szanini@live.fr>
2026-06-18 16:51:29 +0200

    gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // Importez tailwindcss
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite"; // Importez tailwindcss
 
 export default defineConfig({
   plugins: [
     tailwindcss(), // Ajoutez le plugin tailwindcss ici
-    react()
-    base: '/weeb_sylvain_zanini/'
-],
-})
+    react(),
+  ],
+  base: "/weeb_sylvain_zanini/",
+});
